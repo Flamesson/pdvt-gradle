a9f7fdb9bde7fcd0f1ca0667f321458f2a075289
Aiden <a.izumi@haulmont.com>
2023-06-07 12:48:57 +0400

    Fix a bug

--- src/main/java/org/izumi/pdvt/gradle/task/SendableReport.java
+++ src/main/java/org/izumi/pdvt/gradle/task/SendableReport.java
@@ -2,12 +2,13 @@ package org.izumi.pdvt.gradle.task;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.function.Function;
 
+import com.github.mizosoft.methanol.Methanol;
 import com.github.mizosoft.methanol.MultipartBodyPublisher;
+import com.github.mizosoft.methanol.MutableRequest;
 import lombok.RequiredArgsConstructor;
 import org.gradle.api.logging.Logger;
 import org.izumi.pdvt.gradle.File;
@@ -25,11 +26,11 @@ public class SendableReport {
     public void send() {
         final Code code = new Code(parameters.getClientCode().orElseThrow());
 
-        final HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest createUserRequest = HttpRequest.newBuilder()
-                .uri(URI.create(code.getServer() + "/client/" + code.getCodeword() + "/create-if-absent"))
-                .POST(HttpRequest.BodyPublishers.ofString(code.getRaw()))
-                .build();
+        final Methanol client = Methanol.create();
+        final MutableRequest createUserRequest = MutableRequest.POST(
+                URI.create(code.getServer() + "/client/" + code.getCodeword() + "/create-if-absent"),
+                HttpRequest.BodyPublishers.ofString(code.getRaw())
+        );
         final HttpResponse<InputStream> clientCodeCheckResponse = Utils.silently(
                 () -> client.send(createUserRequest, BODY_HANDLER)
         );
@@ -39,17 +40,15 @@ public class SendableReport {
         }
 
         final MultipartBodyPublisher publisher = Utils.silently(() ->
-                MultipartBodyPublisher.newBuilder().filePart("file", file.asPath()).build()
+                MultipartBodyPublisher.newBuilder()
+                        .textPart("code", code.getRaw())
+                        .filePart("file", file.asPath())
+                        .build()
         );
-        //code.getServer() + "/client/" + code.getCodeword() + "/files/add"
-        final HttpRequest uploadFileRequest = HttpRequest.newBuilder()
-                .uri(URI.create(uriSupplier.apply(code)))
-                .POST(publisher)
-                .header("Content-Type", "multipart/form-data; boundary=" + publisher.boundary())
-                .build();
 
+        final MutableRequest request = MutableRequest.POST(URI.create(uriSupplier.apply(code)), publisher);
         Utils.silently(() -> {
-            final HttpResponse<InputStream> response = client.send(uploadFileRequest, BODY_HANDLER);
+            final HttpResponse<InputStream> response = client.send(request, BODY_HANDLER);
             if (response.statusCode() != 200) {
                 logger.lifecycle("Failed to send file to server");
             }
